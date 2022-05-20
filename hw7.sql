2cc27d1f5ac777797ead7c75e0385de5e54b5a22
tmacgregor413 <90424646+tmacgregor413@users.noreply.github.com>
2022-05-20 11:41:38 -0400

    Update hw7.sql

--- hw7.sql
+++ hw7.sql
@@ -1,60 +1,73 @@
-create table class
+  CREATE DATABASE school;
+use school;
+
+Create Table grade
 (
-    id            int auto_increment,
-    class_name    varchar(10) null,
-    class_subject varchar(10) null,
-    constraint id
-        unique (id)
+  id int NOT NULL UNIQUE AUTO_INCREMENT,
+  grade_num tinyint
 );
 
-create table class_roster
+Create Table class
 (
-    id         int auto_increment,
-    class_id   int not null,
-    student_id int not null,
-    constraint id
-        unique (id)
+    id int NOT NULL UNIQUE AUTO_INCREMENT,
+    class_name varchar(10),
+    class_subject varchar(10)
 );
 
-create table grade
+Create Table student_address
 (
-    id        int auto_increment,
-    grade_num tinyint null,
-    constraint id
-        unique (id)
+    id int NOT NULL UNIQUE AUTO_INCREMENT,
+    address varchar(40),
+    town varchar(15),
+    zip smallint,
+    PRIMARY KEY (id)
 );
 
-create table student_address
+Create Table student
 (
-    id      int auto_increment
-        primary key,
-    address varchar(40) null,
-    town    varchar(15) null,
-    zip     smallint    null,
-    constraint id
-        unique (id)
+    id int NOT NULL UNIQUE AUTO_INCREMENT,
+    firstname varchar(15) NOT NULL,
+    lastname varchar(15) NOT NULL,
+    grade_id int,
+    address_id int,
+    FOREIGN KEY (address_id) references student_address(id),
+    FOREIGN KEY (grade_id) references grade(id),
+    PRIMARY KEY (id)
 );
 
-create table student
+Create Table class_roster
 (
-    id         int auto_increment
-        primary key,
-    firstname  varchar(15) not null,
-    lastname   varchar(15) not null,
-    grade_id   int         null,
-    address_id int         null,
-    constraint id
-        unique (id),
-    constraint student_ibfk_1
-        foreign key (address_id) references student_address (id),
-    constraint student_ibfk_2
-        foreign key (grade_id) references grade (id)
+    id int NOT NULL UNIQUE AUTO_INCREMENT,
+    class_id int NOT NULL,
+    student_id int NOT NULL
 );
 
-create index address_id
-    on student (address_id);
+SET FOREIGN_KEY_CHECKS=0;
+INSERT INTO student (firstname, lastname, grade_id) VALUES ('Thomas', 'MacGregor', 3);
+INSERT INTO student (firstname, lastname, grade_id) VALUES ('Gavin', 'Johnston', 5);
+INSERT INTO student (firstname, lastname, grade_id) VALUES ('Sebastian', 'Roy', 1);
+INSERT INTO student (firstname, lastname, grade_id) VALUES ('Patrick', 'Ogden', 2);
+INSERT INTO student (firstname, lastname, grade_id) VALUES ('Veronica', 'Ouellette', 2);
+INSERT INTO student (firstname, lastname, grade_id) VALUES ('Samantha', 'Lawrence', 2);
 
-create index grade_id
-    on student (grade_id);
+INSERT INTO grade (grade_num) VALUE (8);
+INSERT INTO grade (grade_num) VALUE (9);
+INSERT INTO grade (grade_num) VALUE (10);
+INSERT INTO grade (grade_num) VALUE (11);
 
+DELETE FROM grade where id = 6;
+DELETE FROM grade where id = 7;
+DELETE FROM grade where id = 8;
+DELETE FROM grade where id = 9;
+DELETE FROM grade where id in (6, 7, 8, 9, 10);
 
+select * from student;
+select * from class;
+select * from class_roster;
+select * from grade;
+select * from student_address;
+
+SELECT student.firstname, student.lastname, grade.grade_num
+FROM student
+INNER JOIN grade ON student.grade_id = grade.id
+where lastname = 'MacGregor';
